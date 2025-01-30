119cb6c992e65085559ae1b9568fccfb7a02899e
David Selp <dave.selph@gmail.com>
2025-01-30 14:07:49 -0700

    starter commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // scan in the locations;
+        //
+
+
+        try (Scanner scanner = new Scanner(new File("C:\\Users\\daves\\Desktop\\UTEP\\Spring 2025\\Adv. OOP\\Labs\\input_file.txt"))) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
