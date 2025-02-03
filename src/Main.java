02e3f7871585889893eed55fd331def20d1d7412
Torres <adtorres35@miners.utep.edu>
2025-02-02 23:22:01 -0700

    added checks for user input is within the board, and minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,14 +9,14 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{      //added IOexeption
 
         String input_path = "C:\\Users\\adtorres35\\OneDrive - University of Texas at El Paso\\Desktop\\AOOP\\AOOP_lab_grp16\\input_file.txt";
+        //String input_path = "input_file.txt";     //this doesnt seem to work, i belive its because of the school computer i use
 
         //todo sanitize for out of bound numbers and letters
         List<String> piece_info = getTextFileInput(input_path);
 
-        //todo sanitize inputs for out of range chars and numbers
         int[] inputs =  get_user_inputs();
 
         List<ChessPiece> pieces = pieceInfoToChessPiece(piece_info);
@@ -49,13 +51,47 @@ public class Main {
     }
 
     public static int[] get_user_inputs(){
+        Exception input_err = new Exception("user input not within string length or board size");
+        
+        boolean invalid_input = true;
         Scanner user_input = new Scanner(System.in);
-        System.out.println("Enter the Letter for the space you would like the piece to move: ");
-        String x_movement_str = user_input.nextLine().toLowerCase();
-        System.out.println("Enter the number for the space you would like the piece to move: ");
-        String y_movement_str = user_input.nextLine().toLowerCase();
+        String x_movement_str = "";
+        while (invalid_input) {
+            System.out.println("Enter the Letter for a space on the board: ");
+            try {
+                x_movement_str = user_input.nextLine().toLowerCase();
+                if(x_movement_str.length() >1){         //checking for length is kind of redundant since i only use the first char later anyway
+                    throw input_err;
+                }else{
+                    char []user_input_arr = x_movement_str.toCharArray();
+                    if((user_input_arr[0]>='h')||(user_input_arr[0]<'a')){
+                        throw input_err;
+                    }
+                }
+                invalid_input = false;      //escape this mortal coil
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("please enter the valid letter for a space within the board");
+            }
+        }
+        invalid_input = true;
+        int y_movement_int = 0;
+        while(invalid_input){
+            System.out.println("Enter the number for a space on the board: ");
+            try {
+                y_movement_int = user_input.nextInt();
+                if((y_movement_int > 8)||(y_movement_int<1)){
+                    throw input_err;
+                }
+                invalid_input = false;
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("please enter the valid number for a space within the board");
+            }
 
-        return new int[]{letter_to_number(x_movement_str), Integer.parseInt(y_movement_str)};
+        }
+
+        return new int[]{letter_to_number(x_movement_str), y_movement_int};     //changed y_movement form string to int
     }
 
     public static int letter_to_number(String letter) {
@@ -151,11 +187,24 @@ public class Main {
 
         }
     }
+
     
     //helper methods to reduce bulk
+    /**
+     * Print a messge that the desired move is possible
+     * 
+     * @param piece current chess piece
+     * @param inputs the desired move
+     */
     public static void move_succsess(ChessPiece piece, int[] inputs){
         System.out.println("The "+piece.type+" can move from (" + piece.x_cord_str +"," + piece.y_cord_str +") to (" + numberToLetter(inputs[0]) + "," + inputs[1] + ")"  );
     }
+    /**
+     * Print a messge that the desired move is NOT possible
+     * 
+     * @param piece current chess piece
+     * @param inputs the desired move
+     */
     public static void move_failure(ChessPiece piece, int[] inputs){
         System.out.println("The "+piece.type+" can NOT move from (" + piece.x_cord_str +"," + piece.y_cord_str +") to (" + numberToLetter(inputs[0]) + "," + inputs[1] + ")"  );
     }
